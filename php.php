159d923f2771c8eec8bb49acae46b432167ed372
小宇~ <95265298+xiaoyu171222853@users.noreply.github.com>
2023-01-22 11:23:14 +0800

    php语言的兔年快乐

--- php.php
+++ php.php
@@ -3,7 +3,7 @@
  * @Author: xiaoyu 
  * @Date: 2023-01-22 11:07:40 
  * @Last Modified by:   xiaoyu 
- * @Last Modified time: 2023-01-22 11:07:40 
+ * @Last Modified time: 2023-01-22 11:22:59
  */
-    echo("Hello World");
+    echo("兔年快乐");
 ?>
