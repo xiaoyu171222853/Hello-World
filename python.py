853acb693d1fce9625574fd60eba3b88f2306a0a
小宇~ <95265298+xiaoyu171222853@users.noreply.github.com>
2023-01-22 11:23:50 +0800

    python语言的兔年快乐

--- python.py
+++ python.py
@@ -2,6 +2,6 @@
 @ Author: xiaoyu
 @ Date: 2023-01-22 11: 08: 35
 @ Last Modified by:   xiaoyu
-@ Last Modified time: 2023-01-22 11: 08: 35
+@ Last Modified time: 2023-01-22 11: 23: 35
 '''
-print("Hello world")
+print("兔年快乐")
