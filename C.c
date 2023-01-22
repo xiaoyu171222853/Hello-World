500ac66b3d2a21f2d938b26f898aaea994815b91
小宇~ <95265298+xiaoyu171222853@users.noreply.github.com>
2023-01-22 11:21:37 +0800

    C语言的兔年快乐

--- C.c
+++ C.c
@@ -2,11 +2,11 @@
  * @Author: xiaoyu 
  * @Date: 2023-01-22 11:06:59 
  * @Last Modified by:   xiaoyu 
- * @Last Modified time: 2023-01-22 11:06:59 
+ * @Last Modified time: 2023-01-22 11:21:59 
  */
 
 #include <stdio.h>
 void main()
 {
-    printf("Hello World");
+    printf("兔年快乐");
 }
