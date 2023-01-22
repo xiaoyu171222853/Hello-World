68f8fba30486fa8f1b94349abb6a6854680e8033
小宇~ <95265298+xiaoyu171222853@users.noreply.github.com>
2023-01-22 11:22:27 +0800

    Java语言的兔年快乐

--- Java.java
+++ Java.java
@@ -2,11 +2,11 @@
  * @Author: xiaoyu 
  * @Date: 2023-01-22 11:05:42 
  * @Last Modified by:   xiaoyu 
- * @Last Modified time: 2023-01-22 11:05:42 
+ * @Last Modified time: 2023-01-22 11:22:42 
  */
 
 public class Java {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("兔年快乐");
     }
 }
